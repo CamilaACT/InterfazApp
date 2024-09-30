2642bc17281b8e338ab5b8c20fc7abf71c56a643
CamilaPROCELEC <camilacabrera464@gmail.com>
2024-09-30 13:45:40 -0500

    ListoHataDiseñoTcUsuario

--- src/app/Components/layout/Modales/modal-usuario/modal-usuario.component.ts
+++ src/app/Components/layout/Modales/modal-usuario/modal-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { FormBuilder,FormGroup,Validator } from '@angular/forms';
+import { FormBuilder,FormGroup,Validator, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Rol } from '../../../../interfaces/rol';
 import { Usuario } from '../../../../interfaces/usuario';
@@ -16,10 +16,108 @@ import { UtilidadService } from '../../../../Reutilizable/utilidad.service';
 })
 export class ModalUsuarioComponent implements OnInit{
 
-  
+  formularioUsuario:FormGroup;
+  ocultarPassword:boolean=true;
+  tituloAccion:string="Agregar";
+  botonAccion:string="Guardar";
+  listaRoles:Rol[]=[];
 
-  constructor(){}
+  constructor(
+
+    private modalActual:MatDialogRef<ModalUsuarioComponent>,
+    @Inject(MAT_DIALOG_DATA) public datosUsuario: Usuario,
+    private fb:FormBuilder,
+    private _rolServicio:RolService,
+    private _usuarioServicio:UsuarioService,
+    private _utilidadServicio:UtilidadService
+  ){
+    this.formularioUsuario = this.fb.group({
+      nombreCompleto: ["", Validators.required],  // Corregido: Comillas bien colocadas
+      correo: ["", Validators.required], 
+      idRol: ["", Validators.required], 
+      clave: ["", Validators.required], 
+      esActivo: ["1", Validators.required], 
+    });
+
+    if(this.datosUsuario !=null){
+      this.tituloAccion="Editar";
+      this.botonAccion="Actualizar";
+    }
+
+    this._rolServicio.listaRoles(1).subscribe({
+      next:(data)=>{
+        if(data.CodigoError===-1){
+          this.listaRoles=data.Result
+        }
+      },
+      error:(e) =>{}
+    })
+
+
+
+  }
    ngOnInit(): void {
-     
+     if(this.datosUsuario!=null){
+      this.formularioUsuario.patchValue({
+        nombreCompleto: this.datosUsuario.nombre,  // Corregido: Comillas bien colocadas
+        correo: this.datosUsuario.correo, 
+        idRol: this.datosUsuario.idRol, 
+        clave: this.datosUsuario.clave, 
+        esActivo: this.datosUsuario.status , 
+      })
+     }
    }
+
+   guardarEditar_Usuario() {
+    const _usuario: Usuario = {
+      idUsuario: this.datosUsuario == null ? 0 : this.datosUsuario.idUsuario,
+      nombre: this.formularioUsuario.value.nombreCompleto,
+      correo: this.formularioUsuario.value.correo,
+      idRol: this.formularioUsuario.value.idRol,
+      rolDescripcion: "", 
+      clave: this.formularioUsuario.value.clave,
+      status: parseInt(this.formularioUsuario.value.esActivo)  // Asegúrate de que esActivo sea convertible a número
+    }
+
+    if (this.datosUsuario == null) {
+      this._usuarioServicio.guardarUsuario(_usuario).subscribe({
+        next: (data) => {
+          if (data.CodigoError === -1) {  // Verificamos si data.status es verdadero
+            this._utilidadServicio.mostrarAlerta("El usuario fue registrado", "Éxito");
+            this.modalActual.close("true")
+          }else{
+            this._utilidadServicio.mostrarAlerta("Ocurrió un error al registrar el usuario", "Error");
+          }
+        },
+        error: (err) => {
+          this._utilidadServicio.mostrarAlerta("Ocurrió un error al registrar el usuario", "Error Final");
+          console.error(err);  // Opcional, para depurar en caso de error
+        }
+      });
+    }else{
+
+      this._usuarioServicio.editarUsuario(_usuario).subscribe({
+        next: (data) => {
+          if (data.CodigoError === -1) {  // Verificamos si data.status es verdadero
+            this._utilidadServicio.mostrarAlerta("El usuario fue editado", "Éxito");
+            this.modalActual.close("true")
+          }else{
+            this._utilidadServicio.mostrarAlerta("Ocurrió un error al edital el usuario", "Error");
+          }
+        },
+        error: (err) => {
+          this._utilidadServicio.mostrarAlerta("Ocurrió un error al edital el usuario", "Error Final");
+          console.error(err);  // Opcional, para depurar en caso de error
+        }
+      });
+
+
+
+    }
+
+    
+
+
+  }
+  
 }
--- src/app/Components/layout/Pages/usuario/usuario.component.ts
+++ src/app/Components/layout/Pages/usuario/usuario.component.ts
@@ -1,10 +1,110 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit,ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalUsuarioComponent } from '../../Modales/modal-usuario/modal-usuario.component';
+import { Usuario } from '../../../../interfaces/usuario';
+import { UsuarioService } from '../../../../Services/usuario.service';
+import { UtilidadService } from '../../../../Reutilizable/utilidad.service';
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit,AfterViewInit {
+columnasTable:string[]=["nombre","correo","rolDescripcion","estado","acciones"];
+dataInicio:Usuario[]=[];
+dataListaUsarios=new MatTableDataSource(this.dataInicio);
+@ViewChild(MatPaginator) paginacionTabla!:MatPaginator;
+
+constructor(
+  private dialog:MatDialog,
+  private _usuarioServicio:UsuarioService,
+  private _utilidadServicio:UtilidadService
+
+
+){}
+
+obtenerUsuarios(){
+  this._usuarioServicio.listaUsuarios(1).subscribe({
+    next: (data) => {
+      if (data.CodigoError === -1) {  // Verificamos si data.status es verdadero
+        this.dataListaUsarios.data=data.Result
+      }else{
+        this._utilidadServicio.mostrarAlerta("No se encontraron datos", "Error");
+      }
+    },
+    error: (err) => {
+      this._utilidadServicio.mostrarAlerta("Ocurrió un error", "Error Final");
+      console.error(err);  // Opcional, para depurar en caso de error
+}})
+}
+  ngOnInit(): void {
+    this.obtenerUsuarios();
+    
+  }
+  ngAfterViewInit(): void {
+    this.dataListaUsarios.paginator=this.paginacionTabla;
+  }
+
+  aplicarFiltroTabla(event:Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataListaUsarios.filter=filterValue.trim().toLocaleLowerCase();
+  }
+
+  nuevoUsuario(){
+    this.dialog.open(ModalUsuarioComponent,{
+      disableClose:true
+    }).afterClosed().subscribe(resultado=>{
+      if(resultado ==="true")this.obtenerUsuarios();
+    });
+  }
+
+  editarUsuario(usuario:Usuario){
+    this.dialog.open(ModalUsuarioComponent,{
+      disableClose:true,
+      data:usuario
+    }).afterClosed().subscribe(resultado=>{
+      if(resultado ==="true")this.obtenerUsuarios();
+    });
+  }
+  eliminarUsuario(usuario:Usuario){
+    Swal.fire({
+
+      title:"¿Desea eliminar el usuario?",
+      text:usuario.nombre,
+      icon:"warning",
+      confirmButtonColor:"#3085d6",
+      confirmButtonText:"Si",
+      showCancelButton:true,
+      cancelButtonColor: "#d33",
+      cancelButtonText:"No"
+
+
+    }).then((resultado)=>{
+
+      if(resultado.isConfirmed){
+        this._usuarioServicio.eliminarUsuario(usuario.idUsuario).subscribe({
+          next:(data)=>{
+            if(data.CodigoError === -1){
+              this._utilidadServicio.mostrarAlerta("El usuario fue eliminado","Listo!");
+              this.obtenerUsuarios();
+            }else{
+              this._utilidadServicio.mostrarAlerta("NO se puedo eliminar el usuario","Error");
+            }
+          },
+          error:(e)=>{
+            this._utilidadServicio.mostrarAlerta("NO se puedo eliminar el usuario","Error Final");
+          }
+
+        })
+      }
+    })
+
+  }
+
+
+
 
 }
